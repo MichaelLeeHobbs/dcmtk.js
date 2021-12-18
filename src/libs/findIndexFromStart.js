d30c93afbbaf508d7d1b0c6848b386e3953c11c7
Michael Hobbs <michael.lee.hobbs@gmail.com>
2021-12-18 18:19:37 -0500

    added findIndexFromStart needed for parser

---
+++ src/libs/findIndexFromStart.js
@@ -0,0 +1,17 @@
+/**
+ * Same as Array.findIndex but requires a start index
+ * @param {[]} arr
+ * @param {Number} start
+ * @param {function} callbackFn
+ * @return {number}
+ */
+const findIndexFromStart = (arr, start, callbackFn) => {
+  for (let i = start; i < arr.length; i++) {
+    if (callbackFn(arr[i], i, arr)) {
+      return i
+    }
+  }
+  return -1
+}
+
+module.exports = findIndexFromStart
