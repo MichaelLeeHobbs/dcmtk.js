eb665eb24769a3fe870f5cd548a6e0901257052f
Michael Hobbs <michael.lee.hobbs@gmail.com>
2021-12-23 17:34:11 -0500

    added many new events to support DCMRecv.js

--- src/events/index.js
+++ src/events/index.js
@@ -23,9 +23,70 @@ const EVENTS = {
   }),
   sendingEchoRequest: new DCMTKEvent({event: 'sendingEchoRequest', level: 'info', body: /^(?<level>\w): Sending Echo Request \(MsgID (?<msgID>[^]*?)\)$/}),
   receivedEchoResponse: new DCMTKEvent({event: 'receivedEchoResponse', level: 'info', body: /^(?<level>\w): Received Echo Response \((?<status>[^]*?)\)$/}),
-  checkingInputFiles: new DCMTKEvent({event: 'checkingInputFiles', level: 'info', body: /^(?<level>\w): (?<message>sending SOP instances) ...$/}),
-  sendingSOPInstances: new DCMTKEvent({event: 'sendingSOPInstances', level: 'info', body: /^(?<level>\w): (?<message>sending SOP instances) ...$/}),
-  requestingAssociation: new DCMTKEvent({event: 'requestingAssociation', level: 'info', body: /^(?<level>\w): (?<message>Requesting Association)$/}),
+  checkingInputFiles: new DCMTKEvent({event: 'checkingInputFiles', level: 'trace', body: /^(?<level>\w): checking input files ...$/}),
+  initializingNetwork: new DCMTKEvent({event: 'initializingNetwork', level: 'trace', body: /^(?<level>\w): initializing network ...$/}),
+  sendingSOPInstances: new DCMTKEvent({event: 'sendingSOPInstances', level: 'info', body: /^(?<level>\w): sending SOP instances ...$/}),
+  startingSCP: new DCMTKEvent({event: 'startingSCP', level: 'info', body: /^(?<level>\w): starting service class provider and listening ...$/}),
+  loadingConfiguration: new DCMTKEvent({
+    event: 'loadingConfiguration',
+    level: 'info',
+    body: /^(?<level>\w): Loading association configuration file: (?<file>[^]*?)$/
+  }),
+  configuringSCP: new DCMTKEvent({event: 'configuringSCP', level: 'trace', body: /^(?<level>\w): configuring service class provider ...$/}),
+  requestingAssociation: new DCMTKEvent({event: 'requestingAssociation', level: 'info', body: /^(?<level>\w): Requesting Association$/}),
+  associationTerminated: new DCMTKEvent({event: 'associationTerminated', level: 'info', body: /^(?<level>\w): DcmSCP: Association Terminated$/}),
+  cleaningUp: new DCMTKEvent({event: 'associationTerminated', level: 'info', body: /^(?<level>\w): Cleaning up internal association and network structures$/}),
+  determiningInputFiles: new DCMTKEvent({event: 'determiningInputFiles', level: 'trace', body: /^(?<level>\w): determining input files ...$/}),
+  checkingSOPUIDConsistent: new DCMTKEvent({
+    event: 'checkingUIDConsistent',
+    level: 'trace',
+    body: /^(?<level>\w): checking whether SOP Class UID and SOP Instance UID in dataset are consistent with transfer list$/
+  }),
+  gettingSOPUID: new DCMTKEvent({
+    event: 'gettingSOPUID',
+    level: 'trace',
+    body: /^(?<level>\w): getting SOP Class UID, SOP Instance UID and Transfer Syntax UID from DICOM dataset$/
+  }),
+  preparingPresentationContext: new DCMTKEvent({
+    event: 'preparingPresentationContext',
+    level: 'trace',
+    body: /^(?<level>\w): preparing presentation context for SOP Class \/ Transfer Syntax: (?<msgID>[^]*?)$/
+  }),
+  proposeUncompressedTransferSyntaxes: new DCMTKEvent({
+    event: 'preparingPresentationContext',
+    level: 'trace',
+    body: /^(?<level>\w): (?<message>also propose the three uncompressed transfer syntaxes, because we can decompress the SOP instance \(if required\))$/
+  }),
+  addedPresentationContext: new DCMTKEvent({
+    event: 'addedPresentationContext',
+    level: 'trace',
+    body: /^(?<level>\w): added new presentation context with ID (?<id>[^]*?)$/
+  }),
+  configuredPresentationContext: new DCMTKEvent({
+    event: 'configuredPresentationContext',
+    level: 'trace',
+    body: /^(?<level>\w): Configured a total of (?<count>[^]*?) presentation contexts for SCU$/
+  }),
+  startingAssociation: new DCMTKEvent({
+    event: 'startingAssociation',
+    level: 'info',
+    body: /^(?<level>\w): starting association #(?<number>[^]*?)$/
+  }),
+  negotiatingNetworkAssociation: new DCMTKEvent({
+    event: 'negotiatingNetworkAssociation',
+    level: 'info',
+    body: /^(?<level>\w): negotiating network association ...$/
+  }),
+  separator: new DCMTKEvent({
+    event: 'separator',
+    level: 'trace',
+    body: /^(?<level>\w): -|\++$/
+  }),
+  multipleAssociationsAllowed: new DCMTKEvent({
+    event: 'multipleAssociationsAllowed',
+    level: 'trace',
+    body: /^(?<level>\w): multiple associations allowed \(option --multi-associations used\)$/
+  }),
   networkSendTimeout: new DCMTKEvent({
     event: 'networkSendTimeout',
     level: 'trace',
@@ -36,7 +97,16 @@ const EVENTS = {
     level: 'trace',
     body: /^(?<level>\w): (?<message>setting network receive timeout to \d+ seconds)$/
   }),
-  cStoreResponse: new DCMTKEvent({event: 'cStoreResponse', level: 'info', body: /^(?<level>\w): (?<message>Received C-STORE Response)$/}),
+  receivedDatasetPresentationContext: new DCMTKEvent({
+    event: 'receivedDatasetPresentationContext',
+    level: 'info',
+    body: /^(?<level>\w): Received dataset on presentation context (?<id>[^]*?) and stored it directly to file$/
+  }),
+  receivedCStoreResponse: new DCMTKEvent({event: 'cStoreResponse', level: 'info', body: /^(?<level>\w): Received C-STORE Response$/}),
+  receivedCStoreRequest: new DCMTKEvent({event: 'cStoreResponse', level: 'info', body: /^(?<level>\w): Received C-STORE Request$/}),
+  receivedAssociationReleaseRequest: new DCMTKEvent({event: 'cStoreResponse', level: 'info', body: /^(?<level>\w): Received Association Release Request$/}),
+  sendingCStoreRequest: new DCMTKEvent({event: 'sendingCStore', level: 'info', body: /^(?<level>\w): Sending C-STORE Request$/}),
+  sendingCStoreResponse: new DCMTKEvent({event: 'sendingCStore', level: 'info', body: /^(?<level>\w): Sending C-STORE Response$/}),
   sendingSOPInstance: new DCMTKEvent({
     event: 'sendingSOPInstance',
     level: 'info',
@@ -57,10 +127,35 @@ const EVENTS = {
   received: new DCMTKEvent({event: 'received', level: 'info', body: /(?<level>\w): cannot execute command '# "(?<data>[^]*?)"'$/, processor: receivedEvent}),
   associationReceivedInitial: new DCMTKEvent({event: 'associationReceivedInitial', level: 'info', body: /(?<level>\w): Association Received$/}),
   associationReceived: new DCMTKEvent({event: 'associationReceived', level: 'info', body: /(?<level>\w): Association Received: (?<host>[^]*)$/}),
+  associationReceived2: new DCMTKEvent({
+    event: 'associationReceived',
+    level: 'info',
+    body: /(?<level>\w): Association Received (?<host>[^]*): (?<callingAETitle>[^]*) -> (?<calledAETitle>[^]*)$/
+  }),
   associationAcknowledged: new DCMTKEvent({
     event: 'associationAcknowledged',
     level: 'info',
     body: /(?<level>\w): Association Acknowledged \(Max Send PDV: (?<maxSendPDV>[^]*)\)$/
+  }),
+  associationAcknowledged2: new DCMTKEvent({
+    event: 'associationAcknowledged',
+    level: 'info',
+    body: /(?<level>\w): DcmSCP: Association Acknowledged$/
+  }),
+  generatedFilename: new DCMTKEvent({
+    event: 'generatedFilename',
+    level: 'info',
+    body: /(?<level>\w): generated filename for object to be received: (?<filename>[^]*?)$/
+  }),
+  overwritingFile: new DCMTKEvent({
+    event: 'overwriting',
+    level: 'info',
+    body: /(?<level>\w): file already exists, overwriting: (?<filename>[^]*?)$/
+  }),
+  storedFile: new DCMTKEvent({
+    event: 'overwriting',
+    level: 'info',
+    body: /(?<level>\w): Stored received object to file: (?<filename>[^]*?)$/
   }),
   associationRelease: new DCMTKEvent({event: 'associationRelease', level: 'info', body: /(?<level>\w): (?<message>Association Release)$/}),
   creatingSubdirectory: new DCMTKEvent({
@@ -74,8 +169,13 @@ const EVENTS = {
     body: /(?<level>\w): Constructing Associate (?<type>[^]*?) PDU$/
   }),
   empty: new DCMTKEvent({event: 'empty', level: 'trace', body: /(?<level>\w):\s*$/}),
+  checkAndReadPreamble: new DCMTKEvent({
+    event: 'DcmMetaInfo.checkAndReadPreamble',
+    level: 'trace',
+    body: /(?<level>\w): DcmMetaInfo::checkAndReadPreamble\(\) TransferSyntax="(?<transferSyntax>[^]*?)"$/
+  }),
   validateMetaInfo: new DCMTKEvent({
-    event: 'validateMetaInfo',
+    event: 'DcmFileFormat.validateMetaInfo',
     level: 'trace',
     body: /(?<level>\w): DcmFileFormat::validateMetaInfo\(\) found \d+ Elements in DcmMetaInfo 'metinf'$/
   }),
