2ffd6b0501ac5cb3f2ac7b866f8b4264ab5e4040
Michael Hobbs <michael.lee.hobbs@gmail.com>
2021-12-23 12:44:21 -0500

    added debugging to DCMTKParser.js and commented out old parser code - need to test/refactor older uses of parser

--- src/parsers/DCMTKParser.js
+++ src/parsers/DCMTKParser.js
@@ -1,3 +1,4 @@
+const debug = require('debug')('DCMTKParser')
 const DCMTKEvent = require('./DCMTKEvent')
 const events = require('events')
 const findIndexFromStart = require('../libs/findIndexFromStart')
@@ -8,8 +9,11 @@ class DCMTKParser extends events {
 
   constructor(events) {
     super()
+    debug('constructor')
     this.#events = events
     this.reset()
+    this.on('parsed', (msg) => this.#state.messages.push(msg))
+    this.on('parseFailed', (msg) => this.#state.messages.push(msg))
   }
 
   get stdoutLog() {
@@ -20,7 +24,12 @@ class DCMTKParser extends events {
     return this.#state.stderr.log
   }
 
+  get messages() {
+    return this.#state.messages
+  }
+
   reset() {
+    debug('reset')
     this.#state = {
       stdout: {
         buffer: '',
@@ -41,74 +50,81 @@ class DCMTKParser extends events {
   }
 
   #parse(source, _messages = []) {
+    debug('#parse', source)
     let blockParserSelected = false
     const parser = this.#state[source].parser
 
     // handle block parser if active
     if (parser && !this.#parseBlock(source, _messages)) {
+      debug('#parse', 'blocker parser is active and not done')
       return {messages: _messages}
     }
 
-    // start parsing where we left off
+    debug('#parse', 'start parsing where we left off')
     for (let i = this.#state[source].parserPos; i < this.#state[source].log.length; i++) {
       let raw = this.#state[source].log[i]
 
       // there are three types of parsers: regex, func, and block
       this.#events.some(event => {
 
-        if (event instanceof DCMTKEvent) {
-          if (!event.isMultiLine()) {
-            let result = event.parse(i, this.#state[source].log, source)
-            if (result) {
-              return this.emit('parsed', result)
-            }
-          } else if (event.hasHeaderMatch(raw)) {
-            this.#state[source].parser = event
-            // parse block timeout to avoid getting stuck trying to parse unexpected message format
-            this.#state[source].blockHandler = setTimeout(() => this.#parseBlockFailed(source), event.timeout || 1000)
-            return blockParserSelected = true
+        // if (event instanceof DCMTKEvent) {
+        if (!event.isMultiLine()) {
+          debug('#parse', 'parsing single line', event.event)
+          let result = event.parse(i, this.#state[source].log, source)
+          if (result) {
+            debug('#parse', 'parsing single line - result', event.event, result)
+            return this.emit('parsed', result)
           }
-
-        } else if (event.type === 'block') {
-          if (event.header.test(raw)) {
-            this.#state[source].parser = event
-            // parse block timeout to avoid getting stuck trying to parse unexpected message format
-            this.#state[source].blockHandler = setTimeout(() => this.#parseBlockFailed(source), event.timeout || 1000)
-            return blockParserSelected = true
-          }
-        } else {
-          let result
-          if (event.type === 'func') {
-            let data = event.func(raw)
-            if (data) {
-              return this.emit('parsed', {...data, dt: new Date(), event: event.event, source})
-            }
-          } else {
-            let matches = event.regex.exec(raw)
-            if (matches) {
-              result = {...matches.groups, dt: new Date(), event: event.event, source}
-              result.raw = (event.event === 'unhandled') ? matches[0] : undefined
-              return this.emit('parsed', result)
-            }
-          }
+        } else if (event.hasHeaderMatch(raw)) {
+          debug('#parse', 'parsing multi line', event.event)
+          this.#state[source].parser = event
+          // parse block timeout to avoid getting stuck trying to parse unexpected message format
+          this.#state[source].blockHandler = setTimeout(() => this.#parseBlockFailed(source), event.timeout || 1000)
+          return blockParserSelected = true
         }
+        // }
+        // else if (event.type === 'block') {
+        //   if (event.header.test(raw)) {
+        //     this.#state[source].parser = event
+        //     // parse block timeout to avoid getting stuck trying to parse unexpected message format
+        //     this.#state[source].blockHandler = setTimeout(() => this.#parseBlockFailed(source), event.timeout || 1000)
+        //     return blockParserSelected = true
+        //   }
+        // } else {
+        //   let result
+        //   if (event.type === 'func') {
+        //     let data = event.func(raw)
+        //     if (data) {
+        //       return this.emit('parsed', {...data, dt: new Date(), event: event.event, source})
+        //     }
+        //   } else {
+        //     let matches = event.regex.exec(raw)
+        //     if (matches) {
+        //       result = {...matches.groups, dt: new Date(), event: event.event, source}
+        //       result.raw = (event.event === 'unhandled') ? matches[0] : undefined
+        //       return this.emit('parsed', result)
+        //     }
+        //   }
+        // }
       })
       if (blockParserSelected) {
         return this.#parse(source, _messages)
       }
       this.#state[source].parserPos = i + 1
     }
-
+    debug('#parse', 'done parsing for now', this.#state)
     // return {messages: _messages}
   }
 
   #parseBlockClear(source, newParseIndex) {
+    debug('#parseBlockClear', source)
     this.#state[source].parserPos = newParseIndex || ++this.#state[source].parserPos
     this.#state[source].parser = undefined
     clearTimeout(this.#state[source].blockHandler)
   }
 
   #parseBlockFailed(source) {
+    debug('#parseBlockFailed', source)
     const parser = this.#state[source].parser
     const headerIndex = this.#state[source].parserPos
     const maxLength = headerIndex + parser?.maxLines || 2
@@ -117,7 +133,8 @@ class DCMTKParser extends events {
     this.#parseBlockClear(source)
   }
 
-  #parseBlock(source, _messages) {
+  #parseBlock(source, /*_messages*/) {
+    debug('#parseBlock', source)
     const event = this.#state[source].parser
     const headerIndex = this.#state[source].parserPos
     const log = this.#state[source].log
@@ -156,6 +173,7 @@ class DCMTKParser extends events {
 
   handleData(chunk, source) {
     const dataStr = chunk.toString().replace(/\r?\n/g, '\n')
+    debug('handleData', {dataStr, source})
     const currStr = this.#state[source].buffer + dataStr
     const dataArray = currStr.split('\n')
 
